c9b0a06132c48823e7abce46642cb8da9cd3409e
AndVer01 <andver090@gmail.com>
2025-09-24 17:52:17 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,6 @@
 sudo apt update-y
 sudo apt install aircrack-ng python3 hashcat nano -y
-git clone https://github.com/SA-17/capbrute.git cap
+git clone https://github.com/AndVer01/capbrute.git cap
 cd cap
 python3 hy.py
-aircrack-ng -w *txt mp.cap
+aircrack-ng -w *txt a.cap
